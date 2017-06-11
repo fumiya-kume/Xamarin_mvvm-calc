b399a182ad16f28b3a4c097dc63ac2d02c07e02b
Fumiya Kume <fumiya.kume@hotmail.com>
2017-06-11 23:19:23 +0900

    Init project

---
+++ CalcApp/CalcApp/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CalcApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
